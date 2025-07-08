144411b5cf223ef4f40659dcaeb7d649db6e3dec
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2025-07-08 21:25:49 +0530

    Time: 58 ms (66.83%), Space: 121.5 MB (86.06%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,33 +1,17 @@
 class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> a[1] - b[1]); 
-        int n = events.length;
-
-        int[][] dp = new int[n + 1][k + 1];
-
-        for (int i = 1; i <= n; i++) {
-            int[] event = events[i - 1];
-            int prev = binarySearch(events, event[0]);
-
-            for (int j = 1; j <= k; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[prev + 1][j - 1] + event[2]);
-            }
+    public int maxValue(int[][] e, int k) {
+        Arrays.sort(e, (a, b) -> a[0] - b[0]);
+        int n = e.length, dp[][] = new int[n + 1][k + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int l = i + 1, r = n, t = e[i][1], m, next = n;
+            while (l < r)
+                if (e[m = l + (r - l) / 2][0] > t)
+                    r = m;
+                else
+                    l = m + 1;
+            for (int j = 0; j < k; j++)
+                dp[i][j] = Math.max(dp[i + 1][j], dp[l][j + 1] + e[i][2]);
         }
-        return dp[n][k];
-    }
-    private int binarySearch(int[][] events, int currentStart) {
-        int left = 0, right = events.length - 1;
-        int result = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (events[mid][1] < currentStart) {
-                result = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return result;
+        return dp[0][0];
     }
 }
