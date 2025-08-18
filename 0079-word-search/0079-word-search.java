87f84c6d6192c37dfe4b72778ffa123bb74d4d50
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2025-08-18 09:37:51 +0530

    Time: 132 ms (80.17%), Space: 41.6 MB (62.04%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,36 +1,39 @@
 class Solution {
-    private boolean[][] visited;
     public boolean exist(char[][] board, String word) {
         int m = board.length, n = board[0].length;
-        visited = new boolean[m][n];
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0) && 
-                    dfs(board, i, j, 0, word)) return true;
+                if (dfs(board, i, j, 0, word)) return true;
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board, int i, int j, int index, String word) {
+
+    private boolean dfs(char[][] board, int i, int j, int index, String word) {
+        // base case: found full word
         if (index == word.length()) return true;
-        
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || 
-        board[i][j] != word.charAt(index) || visited[i][j]) {
+
+        // boundary + mismatch check
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length 
+            || board[i][j] != word.charAt(index)) {
             return false;
         }
-            
-        visited[i][j] = true;
-        
-        boolean found = 
-        dfs(board, i + 1, j, index + 1, word) ||
-        dfs(board, i - 1, j, index + 1, word) ||
-        dfs(board, i, j + 1, index + 1, word) ||
-        dfs(board, i, j - 1, index + 1, word);
-            
-        visited[i][j] = false;
-		
+
+        // temporarily mark cell as visited
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        // explore neighbors
+        boolean found =
+            dfs(board, i + 1, j, index + 1, word) ||
+            dfs(board, i - 1, j, index + 1, word) ||
+            dfs(board, i, j + 1, index + 1, word) ||
+            dfs(board, i, j - 1, index + 1, word);
+
+        // backtrack (restore)
+        board[i][j] = temp;
+
         return found;
     }
-        
 }
