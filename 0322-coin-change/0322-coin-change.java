3fa737fa8eb484ac9489428c0155ba823bff1918
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2025-10-28 15:39:00 +0530

    Time: 24 ms (32.77%), Space: 44.9 MB (41.63%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,38 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] minCoins = new int[amount + 1];
-        Arrays.fill(minCoins, amount + 1);
-        minCoins[0] = 0;
 
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (i - coins[j] >= 0) {
-                    minCoins[i] = Math.min(minCoins[i], 1 + minCoins[i - coins[j]]);
-                }
-            }
+    int[] dp;
+    public int coinChange(int[] coins, int amount) {
+
+        dp = new int[amount + 1];
+        Arrays.fill(dp, -1);
+        int ans = coinCount(coins, amount);
+        return (ans == Integer.MAX_VALUE) ?  -1 : ans;
+    }
+
+    int coinCount(int[] coins, int amount) {
+
+        if(amount == 0) {
+            return 0;
+        }
+        if(amount < 0) {
+            return Integer.MAX_VALUE;
         }
 
-        return minCoins[amount] != amount + 1 ? minCoins[amount] : -1; 
+        if(dp[amount] != -1) {
+            return dp[amount];
+        }
+
+        int minCoins = Integer.MAX_VALUE;
+        for(int i = 0; i < coins.length; i++) {
+            int ans = coinCount(coins, amount - coins[i]);
+
+            if(ans != Integer.MAX_VALUE) {
+
+                //we have returned 0 in ans, so now we are updating the ans count
+                //hence 1 + ans
+                minCoins = Math.min(minCoins, 1 + ans);
+            }
+        }
+        return dp[amount] = minCoins;
     }
 }
