fc98646c8d626e26842c282bd87ec5cadfd6333f
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2025-12-25 17:08:14 +0530

    Time: 5 ms (74.11%), Space: 46.2 MB (12.35%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        // negative numbers or numbers ending with 0 (except 0) are not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
