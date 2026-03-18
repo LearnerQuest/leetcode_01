c93bf343f6cb60d6ac9b35b28fbbc1c682c071df
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2026-03-18 23:19:13 +0530

    Time: 84 ms (10.59%), Space: 46.7 MB (74.37%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwo(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                temp.next = a;
+                a = a.next;
+            } else {
+                temp.next = b;
+                b = b.next;
+            }
+            temp = temp.next;
+        }
+
+        if (a != null) temp.next = a;
+        else temp.next = b;
+
+        return dummy.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+
+        ListNode ans = null;
+
+        for (int i = 0; i < lists.length; i++) {
+            ans = mergeTwo(ans, lists[i]);
+        }
+
+        return ans;
+    }
+}
