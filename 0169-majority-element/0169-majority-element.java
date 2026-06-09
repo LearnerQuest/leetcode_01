54c89b0abf3f874d252d4e8805ced7d9c51f1be0
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2026-06-09 23:28:52 +0530

    Time: 1 ms (99.89%), Space: 55.9 MB (6.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-        for(int num: nums){
-            map.put(num, map.getOrDefault(num,0)+1);
-        }
-        n = n/2;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getValue()>n){
-                return entry.getKey();
+        int count =0;
+        int candidate =0;
+        for(int num : nums){
+            if(count ==0){
+                candidate = num;
+            }
+            if(num == candidate){
+                count++;
+            }else{
+                count--;
             }
         }
-        return 0;
+        return candidate;
     }
 }
