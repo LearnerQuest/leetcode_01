030bc93124f919b34ac0a9298c182bf4a0746a46
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2026-06-17 22:55:48 +0530

    Time: 6 ms (72.74%), Space: 45.4 MB (27.21%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,23 +1,25 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<Integer> current = new ArrayList<>();
-        Arrays.sort(candidates);
-        helper(candidates, target, 0, current); // no sum, just target
-        return ans;
-    }
-    void helper(int[] candidates, int target, int indx, List<Integer> current) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(current));
+    public static void findcombination(int index, int[] candidates, int target, List<List<Integer>> ans , List<Integer> ds){
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
             return;
         }
-        if (target < 0) return;
-
-        for (int i = indx; i < candidates.length; i++) {
-            if (i > indx && candidates[i] == candidates[i - 1]) continue; // skip duplicates
-            current.add(candidates[i]);
-            helper(candidates, target - candidates[i], i + 1, current);
-            current.remove(current.size() - 1);
+        for(int i=index; i<candidates.length; i++){
+            if(i > index && candidates[i] == candidates[i-1])
+                continue;
+            if(candidates[i] > target)
+                break;
+            ds.add(candidates[i]);
+            findcombination(i+1, candidates, target-candidates[i], ans, ds);
+            ds.remove(ds.size() - 1);
         }
+        
+        
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<>();
+        findcombination(0, candidates, target, ans , new ArrayList<>() );
+        return ans;
     }
 }
