9b9c389f07e80bd793151ce27ff5483331c4e7e8
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2026-06-17 22:40:20 +0530

    Time: 2 ms (95.37%), Space: 45.7 MB (50.23%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,44 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates,target, 0, new ArrayList() , result);
-        return result;
-    }
-    static void backtrack(int [] candidates, int target, int idx,  ArrayList<Integer> temp, List<List<Integer>> result){
-        if (target == 0) {
-            result.add(new ArrayList<>(temp));
+
+    public static void findCombination(int index,
+                                       int target,
+                                       int[] arr,
+                                       List<List<Integer>> ans,
+                                       List<Integer> ds) {
+
+        if (index == arr.length) {
+            if (target == 0) {
+                ans.add(new ArrayList<>(ds));
+            }
             return;
         }
-        if (target < 0 || idx == candidates.length) return;
-        // choose
-        temp.add(candidates[idx]);
-        backtrack(candidates,  target - candidates[idx],idx, temp, result);
-        temp.remove(temp.size() - 1); // unchoose
 
-        // skip
-        backtrack(candidates,  target,idx+1, temp, result);
+        if (arr[index] <= target) {
+            ds.add(arr[index]);
+
+            findCombination(index,
+                            target - arr[index],
+                            arr,
+                            ans,
+                            ds);
+
+            ds.remove(ds.size() - 1);
+        }
+
+        findCombination(index + 1,
+                        target,
+                        arr,
+                        ans,
+                        ds);
+    }
+
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+
+        findCombination(0, target, arr, ans, ds);
+
+        return ans;
     }
 }
