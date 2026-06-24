6e154b9e58b0c36bdb6e0f4c54d174a7abc7dbbd
Aashi Garg <144847050+LearnerQuest@users.noreply.github.com>
2026-06-25 00:45:06 +0530

    Time: 0 ms (100%), Space: 48.3 MB (36.7%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        Stack<Character> st = new Stack<>();
-        for(int c =0; c<s.length; c++){
-            st.push(s[c]);
-            
-        }
-        for (int i = 0; i < s.length; i++) {
-            s[i] = st.pop();
+        int left =0;
+        int right = s.length-1;
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
         }
     }
 }
